a8a142a7810aa3ca4551c6a2ee822d0d4484ec02
Julian <jules89@arcor.de>
2019-03-27 16:35:30 -0400

    added jar files

---
+++ Experiment/generated_files/eaccf_exp/exp_scripts/runClient_Client_0_original_load.sh
@@ -0,0 +1,2 @@
+#!/bin/sh 
+java -cp libs/*:EACCF_client.jar com.yahoo.ycsb.Client -load -db edu.msu.cse.dkvf.ycsbDriver.DKVFDriver -P properties.txt -p clientClassName=edu.msu.cse.accf.client.EACCFClient -p clientConfigFile=client_config  > exp_results/client_Client_0_original_load.result 2> exp_errors/client_Client_0_original_load.error
